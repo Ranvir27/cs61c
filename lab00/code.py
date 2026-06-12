ad9e2c047bb4e79b46d3bee72c03a9792a2eb17a
Ranvir Oswal <ranviroswal2003@berkeley.edu>
2026-06-12 14:38:53 +0530

    Fiizzbuzz

--- lab00/code.py
+++ lab00/code.py
@@ -7,12 +7,10 @@ def get_airspeed_velocity_of(unladen_swallow):
 # pretend there's code here...
 
 def fizzbuzz(num):
-  if str(num) in ["15"]:
-    print(f"{num}: fizzbuzz")
-  elif str(num) in ["3", "6", "9", "12", "15", "18"]:
-    print(f"{num}: fizz")
-  elif str(num) in ["5", "10", "15"]:
-    print(f"{num}: buzz")
+    if num % 3 == 0:
+ 	print(f"{num}: fizz")
+    if num % 5 == 0:
+        print(f"{num}: buzz")	
 
 for i in range(1, 20):
   fizzbuzz(i)
